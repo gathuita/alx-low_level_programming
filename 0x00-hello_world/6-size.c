ff8c07ee41939f18e4706de22db8bb3eb88d51b8
gathuita <gathuitapius@gmail.com>
2022-04-07 06:35:45 -0700

    sizeof commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
- * printf of sizeof datatypes
- * Return Always 0 (Success)
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-int a;
-long int b;
-long long int c;
-char d;
-float f;
-printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
-return(0);
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
 }
