bea31546af3697ee11cb85e0333d373a093c00ff
gathuita <gathuitapius@gmail.com>
2022-04-07 04:14:34 -0700

    puts.c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main Entry point
  *
- * Return Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\" programming is like building a multiligual puzzle");
+puts("\"Programming is like building multilingual puzzle");
 return (0);
 }
