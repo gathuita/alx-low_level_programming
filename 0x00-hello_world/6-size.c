4e2a3eb0896bdc73b0c730a3d99300a16f4fc5c4
gathuita <gathuitapius@gmail.com>
2022-04-07 05:53:41 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 /**
  * main Entry point
- *
+ * printf of sizeof datatypes
  * Return Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 int a;
 long int b;
 long long int c;
 char d;
 float f;
-
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
