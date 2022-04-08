d172208c5499df389a1983d1da61f04289082e58
gathuita <gathuitapius@gmail.com>
2022-04-08 12:27:56 -0700

    Print Alphabet lowercase a-z and uppercase A-Z commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print lowercase a-z and uppercase A-Z using putchar
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lower = 'a';
+	char upper = 'Z';
+
+	while (lower <='z') /*it prints lowercases*/
+	{
+		putchar(lower);
+		lower++;
+	
+	}
+
+	while (upper <= 'Z') /*print uppercase A-Z*/
+	{
+		putchar(upper);
+		upper++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
