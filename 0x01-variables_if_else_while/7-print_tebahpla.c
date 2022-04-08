2f38e889ec15ab2b637830632a323629e993ac8c
gathuita <gathuitapius@gmail.com>
2022-04-08 13:40:16 -0700

    Print alphabets in reverse using putchar commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the lowercase alphabet in reverse
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+
+		alpha--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
