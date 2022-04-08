6682471ada45d3858a62daaabbc9fd17cd84de7a
gathuita <gathuitapius@gmail.com>
2022-04-08 12:41:07 -0700

    Print Alphabet lowercase a-z except q and e commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print all alphabets in lowercase a-z except q and e
+ * Result: Always 0 (Success)
+ */
+int main(void)
+{
+	char = lower;
+	char = 'a';
+
+	while (lower <= 'z' && !=q || !=e)
+	{
+		putchar(lower);
+		lower++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
