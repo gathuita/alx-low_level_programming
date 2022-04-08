c941fcae0889ce6e6146be3d8d0de944d4b9c01f
gathuita <gathuitapius@gmail.com>
2022-04-08 12:12:30 -0700

    Print Alphabet a-z commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,12 @@ int main(void)
 {
 	char alpha = 'a';
 
-	while (alpha = 'a';
-			{
-			putchar(alpha);
-			alpha++;
+	while (alpha = 'z';
+	{
+		putchar(alpha);
+		alpha++;
 
-			}
+	}
 	putchar('\n');
 
 	return(0);
