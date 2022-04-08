dcdb62fe907f12ccaaeb8693712422d01bdd6de8
gathuita <gathuitapius@gmail.com>
2022-04-08 13:15:29 -0700

    Print All numbers base 10 starting with 0 commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio>
+/**
+ * main - print 0 to 9
+ * Return: Always  0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%i, n);
+		n++;
+	}
+	putchar('\n);
+
+	return (0);
+}
