0ff6902d0b51f0d8a888dc94b15405a344ca7992
gathuita <gathuitapius@gmail.com>
2022-04-08 14:34:21 -0700

    Print all possible combinationstwo numbers using putchar commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prrint all possible combinations of two digits
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int n = '10'
+	
+	while (n <= '99')
+	{
+		putchar(n);
+		if (n != '99')
+		{
+			putchar(',');
+
+			putchar(' ');
+
+		}
+		n++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
