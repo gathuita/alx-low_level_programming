da353ed5e4be339343b31edd9dedba8388ef97e1
gathuita <gathuitapius@gmail.com>
2022-04-13 04:26:29 -0700

    create a function that print sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - return 0 letter not lowercase, 1 letter lowercase.
- * @n: is a parameter of function print_sign 
+ * @n: is a parameter of function print_sign
  * Return: Always 0 (Success)
  */
 int print_sign(int n)
