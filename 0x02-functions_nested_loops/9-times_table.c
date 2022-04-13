310568f58397649ace4a1efb4a4d9a09996e521d
gathuita <gathuitapius@gmail.com>
2022-04-13 04:06:13 -0700

    print functions and nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -36,32 +36,20 @@ if (b < 9)
 
 {
 _putchar(',');
-
 _putchar (' ');
-
 }
-
 }
-
 else
 {
 _putchar ((c / 10) + '0');
-
 _putchar ((c % 10) + '0');
-
 if (b < 9)
-
 {
-
 _putchar(',');
-
 _putchar (' ');
-
 }
 }
 }
 _putchar ('\n');
-
 }
-
 }
