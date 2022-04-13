773fb58efd5fb5098462942e2497cd12b8a789cb
gathuita <gathuitapius@gmail.com>
2022-04-13 04:22:08 -0700

    create a function that print every min of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,32 +7,18 @@
  */
 
 void jack_bauer(void)
-
 {
-int a;
-int b;
-
+int a, b;
 for (a = 0; a <= 23; a++)
 {
-
 for (b = 0; b <= 59; b++)
 {
-
 _putchar (a / 10 + '0');
-
 _putchar (a % 10 + '0');
-
 _putchar (':');
-
 _putchar (b / 10 + '0');
-
 _putchar (b % 10 + '0');
-
 _putchar ('\n');
-
 }
-
-
 }
-
 } 
