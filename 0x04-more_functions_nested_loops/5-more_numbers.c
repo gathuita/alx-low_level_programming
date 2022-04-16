5fa2a0f9d8f1dd197fd70efc1893a1cabcf50f1c
gathuita <gathuitapius@gmail.com>
2022-04-16 12:54:30 -0700

    Print numbers 0 to 14 for 10 time

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints the numbers 0 to 14 for 10 times
+ */
+
+void print_most_numbers(void)
+{
+        int i, j;
+
+        for (i = 0; i <= 14; i++)
+        {
+        _putchar(i);
+		for (j = 0; j <= 10; j++)
+		{
+		_putchar()
+		}
+        }
+        _putchar(10);
+}
+
