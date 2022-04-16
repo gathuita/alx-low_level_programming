e7035c293fc72eb60e34eaf63595872bb2c03bb9
gathuita <gathuitapius@gmail.com>
2022-04-16 11:45:00 -0700

    Is digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 
 /**
  * _isdigit - checks for a digit 0 to 9
- * @digit: - the variable to be checked
+ * @digit: the variable to be checked
  * Return: 1 if its a digit else return 0
  */
 int _isdigit(int digit)
