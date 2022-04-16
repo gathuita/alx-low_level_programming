ff97c7e18d5921631d1655b586e292aa6a0c84be
gathuita <gathuitapius@gmail.com>
2022-04-16 12:03:59 -0700

    Multiplying two integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,13 +2,15 @@
 
 /**
  * mul - multiplies two integers
- * @num1: the first integer
- * @num2: the second integer
- * Return: Always 0 id success
+ * @num1: int to be multiplied to num2
+ * @num2: int to be multiplied to num1
+ * Return: the result of the operation
  */
 int mul(int num1, int num2)
 {
 	int result;
+
 	result = num1 * num2;
+
 	return (result);
 }
