40067a9d2f36cb520214d68309af4ad35914e493
gathuita <gathuitapius@gmail.com>
2022-04-20 05:42:11 -0700

    Length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - the length of a string
+ * @s: is the string pointer.
+ * Return: always 0
+ * 
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
+
+	{
+		len++;
+	}
+
+	return (len);
+
+}
