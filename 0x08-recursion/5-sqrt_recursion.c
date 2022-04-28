f04ffd9f48b7768a085e6d5f6c738ea251b27f3a
gathuita <gathuitapius@gmail.com>
2022-04-28 07:30:05 -0700

    Returns the natural sqrt of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#includei "main.h"
 /**
  * _sqrt_recursion - returns the natural square root of a number.
  * @n: is the sqrt parameter.
@@ -10,9 +10,9 @@ int _sqrt_recursion(int n)
 	{
 		return (-1);
 	}
-	if (n >= 0)
+	if (n == 1)
 	{
-		return (0);
+		return (1);
 	}
 	return (_sqrt_recursion(n));
 }
