89b18a96d3c5b863aed420b53cb7898fbc8b84b1
gathuita <gathuitapius@gmail.com>
2022-04-28 07:49:58 -0700

    Returns prime numbers number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,13 +8,13 @@
  * Return: On success 1.
  * else -1 is returned.
  */
-int prime2(int a, int b)
+int prime(int a, int b)
 {
 	if (a == b)
 		return (1);
 	else if (a % b == 0)
 		return (0);
-	return (prime2(a, b + 1));
+	return (prime(a, b + 1));
 }
 /**
  * is_prime_number - checks if a number is prime number.
