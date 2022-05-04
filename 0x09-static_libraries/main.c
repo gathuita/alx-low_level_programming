ece2bda3835a4aa5fb3845c001a11de26add9a64
gathuita <gathuitapius@gmail.com>
2022-05-04 02:08:06 -0700

    Static libraries

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _puts - prints the quote.
+ 
+ *@s: is the pointer to the string.
+ *Return: 0 on success. always.
+ */
 void _puts(char *s)
 {
     _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
