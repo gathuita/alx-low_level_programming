9ce231518bdae12e9be208e20c8e7aadc9fb84f8
gathuita <gathuitapius@gmail.com>
2022-05-04 02:43:44 -0700

    Create static libraries

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,18 +1,12 @@
 #include "main.h"
 
 /**
- * _puts - prints the quote.
- 
- *@s: is the pointer to the string.
+ *main - prints the quote.
+
  *Return: 0 on success. always.
  */
-void _puts(char *s)
-{
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return;
-}
-
 int main()
 {
-	return 0;
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
 }
