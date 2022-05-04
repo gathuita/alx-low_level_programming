374fc1154b4e7942a1002bed6a99cb3d25d6a62f
gathuita <gathuitapius@gmail.com>
2022-05-04 02:02:49 -0700

    Static libraries

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+void _puts(char *s)
+{
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return;
+}
+
+int main()
+{
+	return 0;
+}
